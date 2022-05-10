59eccb70e70b480b8602cba305b0ec1049cebbae
HCC1207 <flyr1207@gmail.com>
2022-05-10 22:35:21 +0800

    1.0.4

--- app.py
+++ app.py
@@ -13,13 +13,37 @@ def home():
 def news():
     return render_template("news.html")
 
+@app.route("/baseTest")
+def baseTest():
+    return render_template("baseTest.html")
+
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
 @app.route("/pricing")
 def pricing():
     return render_template("pricing.html")
 
-@app.route("/baseTest")
-def baseTest():
-    return render_template("baseTest.html")
+@app.route("/administration")
+def administration():
+    return render_template("administration.html")
+
+@app.route("/academic")
+def academic():
+    return render_template("academic.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/recruit")
+def recruit():
+    return render_template("recruit.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 
